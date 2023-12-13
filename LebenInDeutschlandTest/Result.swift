4222ab107c28132e6ea97eef53deeb3dbf316c88
mehmetKargaci <mehmetkargaci34@gmail.com>
2023-12-13 22:19:01 +0100

    questions with bilders

--- LebenInDeutschlandTest/Result.swift
+++ LebenInDeutschlandTest/Result.swift
@@ -1,28 +1,8 @@
 //
-//  Question.swift
+//  Result.swift
 //  LebenInDeutschlandTest
 //
-//  Created by Mehmet on 03.11.23.
+//  Created by Mehmet on 12.12.23.
 //
 
 import Foundation
-
-struct Result: Codable {
-    let data: [ResultItem]
-    
-}
-
-struct ResultItem: Codable {
-    let question: String
-    let answers: [String]
-    let hasPhoto: Bool
-    let correctAnswerIndex: Int
-    let theme: String
-    let id: Int
-    let correctAnswer: String
-}
-
-
-
-    
-
